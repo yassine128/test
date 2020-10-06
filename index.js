bd282121a69f1c3060f6c7a568666aec8940e1b3
Seddaoui Yassine <yassineseddaoui@gmail.com>
2020-10-06 18:13:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var qrLastSupper = document.createElement("img");
-qrLastSupper.src = "test/images/test0.png"
+qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test0.png"
 document.body.appendChild(qrLastSupper);
