69024d89b43f44457bdfa13e294b24f093591601
Seddaoui Yassine <yassineseddaoui@gmail.com>
2020-10-08 11:01:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var x = Math.floor(Math.random() * 2);
 var qrLastSupper = document.createElement("img");
 qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"
-document.body.appendChild(qrLastSupper);
+document.body.appendChild(qrLastSupper).style.left = "800px";
