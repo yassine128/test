5ddfc39a79fa13b3950b99491331bf3af8fd494f
Seddaoui Yassine <yassineseddaoui@gmail.com>
2020-10-08 11:00:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var x = Math.floor(Math.random() * 2);
 var qrLastSupper = document.createElement("img");
-#qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"
-#document.body.appendChild(qrLastSupper);
-var e = document.images["https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"].style;
-e.position = "absolute";
-e.offsetLeft = "60px";
-e.offsetTop = "20px";
+qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"
+document.body.appendChild(qrLastSupper);
