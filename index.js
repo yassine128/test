4240a024ecf5a42565f28498dfa18fc7336b2241
Seddaoui Yassine <yassineseddaoui@gmail.com>
2020-10-08 11:11:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var x = Math.floor(Math.random() * 2);
-var qrLastSupper = document.createElement("img");
-qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"
-document.body.appendChild(qrLastSupper).style.left = "800px";
+#var qrLastSupper = document.createElement("img");
+#qrLastSupper.src = "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true"
+#document.body.appendChild(qrLastSupper).style.left = "800px";
+
+
+var x = document.createElement("IMG");
+x.setAttribute("src", "https://github.com/yassine128/test/blob/main/images/test".concat(x)+".png?raw=true");
+x.setAttribute("width", "304");
+x.setAttribute("height", "228");
+x.setAttribute("alt", "The Pulpit Rock");
+document.body.appendChild(x);
